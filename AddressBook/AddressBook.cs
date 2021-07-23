9a57103cfac96e0247cf9e59d368caf91902f243
ahamedsahib <adsahib39@gmail.com>
2021-07-23 23:24:54 +0530

    [ahamed]Try to avoid Duplicates

--- AddressBook/AddressBook.cs
+++ AddressBook/AddressBook.cs
@@ -14,23 +14,45 @@ namespace AddressBook
             Console.WriteLine("Enter The Name of the Address Book");
             string addressBookName = Console.ReadLine();
             //Checking uniqueness of address books
-            while (flag)
+            while (addressBookDictionary.Count>0)
             {
-                if (addressBookDictionary.Count > 0)
+                if (addressBookDictionary.ContainsKey(addressBookName))
                 {
-                    if (addressBookDictionary.ContainsKey(addressBookName))
+                    Console.WriteLine("This name of address book already exists");
+                    flag = false;
+                    break;
+                }
+            }
+            
+            while (flag)
+            { 
+                ContactPerson person = new ContactPerson();
+                while (flag)
+                {
+                    Console.Write("Enter First Name: ");
+                    string firstName = Console.ReadLine();
+                    if (contacts.Count >0)
                     {
-                        Console.WriteLine("This name of address book already exists");
+                        var x = contacts.Find(x => x.firstName.Equals(firstName));
+                        if (x != null)
+                        {
+                            Console.WriteLine("Your name  already exists");
+                        }
+                        else
+                        {
+                            person.firstName = firstName;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        person.firstName = firstName;
                         break;
                     }
 
-                }
+                }    //ContactPerson person = new ContactPerson();
 
-
-                ContactPerson person = new ContactPerson();
-
-                Console.Write("Enter First Name: ");
-                person.firstName = Console.ReadLine();
+                
                 Console.Write("Enter Last Name: ");
                 person.lastName = Console.ReadLine();
                 Console.Write("Enter Address: ");
